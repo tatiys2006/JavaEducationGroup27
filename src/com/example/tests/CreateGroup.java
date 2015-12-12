d2090b7a1d3c86a476a699d5cf4ac7356893d2ec
tatiys2006 <tatiys2006@gmail.com>
2015-12-12 18:23:13 +0200

    CreateGroupPassed
    Homework - create Group - passed 2 test

--- src/com/example/tests/CreateGroup.java
+++ src/com/example/tests/CreateGroup.java
@@ -1,28 +1,8 @@
 package com.example.tests;
 
-import java.util.regex.Pattern;
-import java.util.concurrent.TimeUnit;
-import static org.hamcrest.CoreMatchers.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class CreateGroup {
-	private static WebDriver driver;
-	private static String baseUrl;
-	private static boolean acceptNextAlert = true;
-	private static StringBuffer verificationErrors = new StringBuffer();
-
-	@BeforeClass
-	public void setUp() throws Exception {
-		driver = new FirefoxDriver();
-		baseUrl = "http://localhost/";
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	}
-
+public class CreateGroup extends TestBase {
 	@Test
 	public void testNonEmptyTestCreation() throws Exception {
 		openMainPage();
@@ -46,81 +26,5 @@ public class CreateGroup {
 		fillGroupForm(group);
 		submitGroupForm();
 		returnToGroupPage();
-	}
-
-	private void returnToGroupPage() {
-		driver.findElement(By.linkText("group page")).click();
-	}
-
-	private void submitGroupForm() {
-		driver.findElement(By.name("submit")).click();
-	}
-
-	private void fillGroupForm(GroupData group) {
-		driver.findElement(By.name("group_name")).clear();
-		driver.findElement(By.name("group_name")).sendKeys(group.groupName);
-		driver.findElement(By.name("group_header")).clear();
-		driver.findElement(By.name("group_header")).sendKeys(group.header);
-		driver.findElement(By.name("group_footer")).clear();
-		driver.findElement(By.name("group_footer")).sendKeys(group.footer);
-	}
-
-	private void initNewGroup() {
-		driver.findElement(By.name("new")).click();
-	}
-
-	private void gotoGroupPage() {
-		driver.findElement(By.linkText("groups")).click();
-	}
-
-	private void openMainPage() {
-		driver.get(baseUrl + "/addressbookv4.1.4/");
-	}
-
-	@AfterClass
-	public void tearDown() throws Exception {
-		driver.quit();
-		String verificationErrorString = verificationErrors.toString();
-		if (!"".equals(verificationErrorString)) {
-			fail(verificationErrorString);
-		}
-	}
-
-	private void fail(String verificationErrorString) {
-		// TODO Auto-generated method stub
-
-	}
-
-	private boolean isElementPresent(By by) {
-		try {
-			driver.findElement(by);
-			return true;
-		} catch (NoSuchElementException e) {
-			return false;
-		}
-	}
-
-	private boolean isAlertPresent() {
-		try {
-			driver.switchTo().alert();
-			return true;
-		} catch (NoAlertPresentException e) {
-			return false;
-		}
-	}
-
-	private String closeAlertAndGetItsText() {
-		try {
-			Alert alert = driver.switchTo().alert();
-			String alertText = alert.getText();
-			if (acceptNextAlert) {
-				alert.accept();
-			} else {
-				alert.dismiss();
-			}
-			return alertText;
-		} finally {
-			acceptNextAlert = true;
-		}
 	}
 }
