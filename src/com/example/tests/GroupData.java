c4933499557ea81485c3acde8cb5ed313da135f9
tatiys2006 <tatiys2006@gmail.com>
2015-12-12 17:25:23 +0200

    Start
    First commit

---
+++ src/com/example/tests/GroupData.java
@@ -0,0 +1,16 @@
+package com.example.tests;
+
+public class GroupData {
+	public String groupName;
+	public String header;
+	public String footer;
+
+	public GroupData() {
+	}
+
+	public GroupData(String groupName, String header, String footer) {
+		this.groupName = groupName;
+		this.header = header;
+		this.footer = footer;
+	}
+}
